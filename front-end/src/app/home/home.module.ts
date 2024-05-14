4d0963e9f043df635daa1d84db4500ecc769b711
Samuel L P Duque <samuellocatel@gmail.com>
2024-05-14 20:09:30 -0300

    ajuste da home (lista de progressão)

--- front-end/src/app/home/home.module.ts
+++ front-end/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './auth/login/login.component';
+import { CadastroProfissionalComponent } from './auth/cadastro-profissional/cadastro-profissional.component';
 
 
 
@@ -10,6 +11,7 @@ import { LoginComponent } from './auth/login/login.component';
   declarations: [
     HomeComponent,
     LoginComponent,
+    CadastroProfissionalComponent,
   ],
   imports: [
     CommonModule,

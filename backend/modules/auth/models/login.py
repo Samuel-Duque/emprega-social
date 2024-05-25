59a51e68e22ccaa7b6d2d7b90318d9fc9e701731
Tiago Cunha <tiagomscunha@outlook.com>
2024-05-25 02:38:15 -0300

    Melhorias backend

--- backend/modules/auth/models/login.py
+++ backend/modules/auth/models/login.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Login(BaseModel):
-    email: str
-    password: str
+    email: str = Field(..., example="O email do usuário")
+    password: str = Field(..., example="A senha do usuário")
 
     model_config = {
         'json_schema_extra': {
             "example": {
                 "email": "admin@gmail.com",
-                "password": "1234"
+                "password": "1234",
             }
-        }
+        },
     }

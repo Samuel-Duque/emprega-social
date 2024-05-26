7b8dbe0b7db9b148bd65e16567a28ea7b992289e
Tiago Cunha <tiagomscunha@outlook.com>
2024-05-25 23:38:48 -0300

    Adiciona verifySession ao AuthService

--- front-end/src/app/shared/services/auth.service.ts
+++ front-end/src/app/shared/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Login, LoginErrorResponse, LoginResponse } from '../interfaces/login';
+import { Login, LoginResponse } from '../interfaces/login';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../assets/environments/environment';
 import { Observable } from 'rxjs';
@@ -12,5 +12,9 @@ export class AuthService {
 
   signIn(data: Login): Observable<any> {
     return this.http.post(environment.apiUrl + '/auth/login', data)
+  }
+
+  verifySession() {
+    return this.http.get(environment.apiUrl + '/auth/verify')
   }
 }

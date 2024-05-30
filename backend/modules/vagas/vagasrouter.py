baac4ffe0e2a8ba8c1b1e088d716250e4fb398f5
Vitor Gabriel <gabriel.4815.7@gmail.com>
2024-05-30 13:20:25 -0300

    fix: atualizar vaga, criar vaga

--- backend/modules/vagas/vagasrouter.py
+++ backend/modules/vagas/vagasrouter.py
@@ -4,6 +4,7 @@ from modules.vagas.vagasservice import VagasService
 from core.supabase import get_supabase_client
 from core.auth import validate_jwt
 from models.vaga import Vaga
+from modules.auth.authrouter import supabase_session
 
 router = APIRouter(tags=["Vagas"])
 
@@ -16,9 +17,9 @@ async def obter_vaga(estado: str, cidade: str, supabase = Depends(get_supabase_c
     return await VagasService.obter_vaga(estado, cidade, supabase)
 
 @router.post("/criar")
-async def criar_vaga(vaga: Vaga, token: str = Depends(validate_jwt)):
-    return await VagasService.criar_vaga(vaga, token)
+async def criar_vaga(vaga: Vaga, supabase = Depends(get_supabase_client)):
+    return await VagasService.criar_vaga(vaga, supabase)
     
 @router.put("atualizar")
-async def atualizar_vaga(id: int, vaga: Vaga):
-    return {'id': id, 'vaga': vaga}
+async def atualizar_vaga(id: int, vaga: Vaga, supabase = Depends(supabase_session)):
+    return await VagasService.atualizar_vaga(id, vaga, supabase)

4f04fbcf23e94feb0edb0922d2765b78a7f34182
Tiago Cunha <tiagomscunha@outlook.com>
2024-05-31 12:22:08 -0300

    url da api na producao

--- front-end/src/assets/environments/environment.prod.ts
+++ front-end/src/assets/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/v1',
+  production: true,
+  apiUrl: 'superb-generous-louse.ngrok-free.app/api/v1',
   ibgeUfApi: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
   ibgeCityApi: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios',
 };
